cd41c3d9ae37d773852d69589a9c4caceb03d294
Andrey Tramon <tramonandrey@gmail.com>
2017-02-21 21:22:52 +0200

    Fixed Database issue

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,28 +1,68 @@
--- Table: users
-CREATE TABLE users (
-  id       BIGSERIAL    NOT NULL  PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
+-- Sequence: public.hibernate_sequence
+CREATE SEQUENCE public.hibernate_sequence
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 180
+  CACHE 1;
 
--- Table: roles
-CREATE TABLE roles (
-  id   BIGSERIAL    NOT NULL PRIMARY KEY,
-  username VARCHAR(100) NOT NULL
-);
+-- Sequence: public.roles_id_seq
+CREATE SEQUENCE public.roles_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 3
+  CACHE 1;
 
--- Table for mapping user and roles: user_roles
-CREATE TABLE user_roles (
-  user_id INT NOT NULL,
-  role_id INT NOT NULL,
+-- Sequence: public.users_id_seq
+CREATE SEQUENCE public.users_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 10
+  CACHE 1;
 
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (role_id) REFERENCES roles (id),
+-- Table: public.users
+CREATE TABLE public.users
+(
+  id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+  username character varying(255) NOT NULL,
+  password character varying(255) NOT NULL,
+  CONSTRAINT users_pkey PRIMARY KEY (id)
+)
 
-  UNIQUE (user_id, role_id)
-);
+-- Table: public.roles
+CREATE TABLE public.roles
+(
+  id integer NOT NULL DEFAULT nextval('roles_id_seq'::regclass),
+  name character varying(100) NOT NULL,
+  CONSTRAINT roles_pkey PRIMARY KEY (id)
+)
 
--- Insert data
+-- Table: public.user_roles
+CREATE TABLE public.user_roles
+(
+  user_id integer NOT NULL,
+  role_id integer NOT NULL,
+  CONSTRAINT user_roles_role_id_fkey FOREIGN KEY (role_id)
+      REFERENCES public.roles (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT user_roles_user_id_fkey FOREIGN KEY (user_id)
+      REFERENCES public.users (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT user_roles_user_id_role_id_key UNIQUE (user_id, role_id)
+)
+
+-- Table: public.products
+CREATE TABLE public.products
+(
+  id integer NOT NULL,
+  brand character varying(255),
+  model character varying(255) NOT NULL,
+  description character varying(255),
+  cost integer,
+  CONSTRAINT id PRIMARY KEY (id)
+)
 
 INSERT INTO users VALUES (1, 'root', '12345678');
 INSERT INTO users VALUES (1, 'admin', '12345678');
@@ -32,10 +72,4 @@ INSERT INTO roles VALUES (2, 'ROLE_ADMIN');
 
 INSERT INTO user_roles VALUES (1, 2);
 
-CREATE TABLE products (
-  id          SERIAL  PRIMARY KEY   NOT NULL,
-  brand       VARCHAR(50),
-  model       VARCHAR(100)          NOT NULL,
-  description VARCHAR(255),
-  cost INT
-);
+
